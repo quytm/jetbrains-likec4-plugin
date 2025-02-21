19607867ba317df7ad482cc4c954b4c54d28df66
GitHub Action <action@github.com>
2025-02-21 02:38:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/quytm/jetbrainslikec4plugin/MyBundle.kt
+++ src/main/kotlin/com/github/quytm/jetbrainslikec4plugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.quytm.jetbrainslikec4plugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/quytm/jetbrainslikec4plugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/quytm/jetbrainslikec4plugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.quytm.jetbrainslikec4plugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
